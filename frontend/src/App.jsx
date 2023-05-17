d569ffb04de7825d82b508346611ce59e6b75437
Daniel Yang <wyan345@aucklanduni.ac.nz>
2023-05-17 23:53:28 +1200

    Set up Title

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+import Header from './component/Header/Header';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
